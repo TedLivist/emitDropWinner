4ca1f6bb8880011d2824748f1511db4922c32de5
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2025-01-28 22:55:32 +0100

    Trime ERC20 ABI

--- scripts/drop.js
+++ scripts/drop.js
@@ -4,9 +4,7 @@ const TEDDY_COMIC_ADDRESS = "0x1Ac0135935AefE381EECAEedb0944Df1210cdf07";
 const CONTRACT_ADDRESS = "0xa86400895b43a7aad222767dd52a28636ca3e72e";
 
 const ERC20_ABI = [
-  "function approve(address spender, uint256 amount) external returns (bool)",
-  "function transfer(address to, uint256 amount) external returns (bool)",
-  "function transferFrom(address from, address to, uint256 amount) external returns (bool)"
+  "function approve(address spender, uint256 amount) external returns (bool)"
 ];
 
 async function main() {
